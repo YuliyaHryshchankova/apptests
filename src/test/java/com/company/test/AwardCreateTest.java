1c092a697f245394f3f4c21b0898a2308c14e929
YuliyaHryshchankova <yuliya_hryshchankova@epam.com>
2018-05-06 19:16:02 +0300

    AwardCreationTest

---
+++ src/test/java/com/company/test/AwardCreateTest.java
@@ -0,0 +1,81 @@
+package com.company.test;
+
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AwardCreateTest extends PreConfig {
+
+    private static final String URL = "https://staging-web1.corp.globoforce.com/microsites/t/home?client=testclientclone3&setCAG=true";
+    private String username = "E2029318";
+    private String password = "password1";
+    private String titleOfAward = "test title";
+    private String message = "test message";
+
+    @Test(description = "Successful login", priority = 1)
+    public void successLogin() {
+        driver.navigate().to(URL);
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.id("signIn-button")).click();
+        //check whether Welcome page with Privacy Policy and Terms of Use is opened. If YES, click 'Okay - Let's go' button.
+        if (!driver.findElements( By.xpath("//button[@class='btn btn--primary']") ).isEmpty()){
+            driver.findElement(By.xpath("//button[@class='btn btn--primary']")).click();
+         }
+
+        Assert.assertTrue(driver.findElement(By.xpath("//a[@href='/microsites/t/home?client=testclientclone3' and @target='_self']")).isDisplayed(), "Login Failed");
+    }
+
+
+    @Test(description = "Verify Award Creation", priority = 2, dependsOnMethods = "successLogin")
+    public void verifyAwardCreate() {
+
+        driver.findElement(By.xpath("//a[@id='np_start']")).click();
+        WebElement nominateAsMyself = waitElementToBeClickable(By.xpath("//button[@class='js-np-recipient-item gf-btn btn-primary btn-lg']"));
+        nominateAsMyself.click();
+
+        WebElement nominee = waitElementToBeClickable(By.xpath("//ul[@class='np-recipient-list list-unstyled clearfix']/li/div/h4[@data-title='Tony Kozlowsky']"));
+        nominee.click();
+
+        WebElement nextButton = waitElementToBeClickable(By.xpath("//button[@class='js-np-next gf-btn btn-primary btn-block btn-lg inline-block']"));
+        nextButton.click();
+
+        WebElement awardProgram = waitElementToBeClickable(By.xpath("//div[@class='js-np-award-program-item np-award-program-default np-award-program relative']"));
+        awardProgram.click();
+
+        WebElement awardReason = waitElementToBeClickable(By.xpath("//*[@id='np_awardReasons']/div/div/div[1]/div/div[1]/div/div[2]"));
+        awardReason.click();
+
+        WebElement skipAwardAdvisor = waitElementToBeClickable(By.xpath("//a[@class='js-np-skip-advisor block np-advisor-skip-link award_reason_1_color']"));
+        skipAwardAdvisor.click();
+
+        WebElement awardType = waitElementToBeClickable(By.xpath("//div[3]/bdi"));
+        awardType.click();
+
+        WebElement awardTitle = waitElementToBeClickable(By.xpath("//textarea[@id='np_awardTitle']"));
+        awardTitle.sendKeys(titleOfAward);
+
+        WebElement awardMessage = waitElementToBeClickable(By.xpath("//textarea[@id='np_awardMessage']"));
+        awardMessage.sendKeys(message);
+
+        WebElement sendAward = waitElementToBeClickable(By.xpath("//button[@class='js-np-send-award gf-btn btn-primary btn-lg btn-block inline-block']"));
+        sendAward.click();
+
+        WebElement closeButton = waitElementToBeClickable(By.xpath("//button[@class='js-np-close gf-btn btn-primary btn-block btn-lg inline-block']"));
+        Assert.assertTrue(driver.findElement(By.xpath("//button[@class='js-np-close gf-btn btn-primary btn-block btn-lg inline-block']")).isDisplayed(), "Award Creation Failed");
+        closeButton.click();
+
+
+    }
+
+
+    @Test(description = "Successful logout", priority = 3, dependsOnMethods = "verifyAwardCreate")
+    public void successLogOut() {
+        WebElement logOut = waitElementToBeClickable(By.xpath("//a[@href='/microsites/t/home?logout&client=testclientclone3']"));
+        logOut.click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id='signIn-button']")).isDisplayed(), "Logout Failed");
+    }
+
+
+}
---
+++ src/test/java/com/company/test/PreConfig.java
@@ -0,0 +1,55 @@
+package com.company.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class PreConfig {
+
+    protected WebDriver driver;
+
+    protected static final int IMPLICIT_TIMEOUT = 20;
+
+    @Parameters("browser")
+
+    @BeforeClass(description = "Set up driver depending on browser")
+    public void startDriver(@Optional("firefox") String browser) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", ".\\src\\test\\resources\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("ie")) {
+            System.setProperty("webdriver.edge.driver", ".\\src\\test\\resources\\IEDriverServer.exe");
+            driver = new EdgeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT, TimeUnit.SECONDS);
+    }
+
+
+    @AfterClass(description = "Quit browser", alwaysRun = true)
+    public void closeDriver() {
+        if (driver != null)
+            driver.quit();
+    }
+
+    protected WebElement waitElementToBeClickable(final By by) {
+        return new WebDriverWait(driver, 20)
+                .pollingEvery(Duration.ofSeconds(1))
+                .withMessage("Failed to wait element: " + by)
+                .until(ExpectedConditions.elementToBeClickable(by));
+    }
+}
