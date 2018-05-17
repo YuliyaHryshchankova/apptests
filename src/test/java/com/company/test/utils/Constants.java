824c1976e6987d3813dac76d49a10fc0c8f9cb7a
YuliyaHryshchankova <yuliya_hryshchankova@epam.com>
2018-05-17 10:15:39 +0300

    - utils package added - Constants interface added - AwardCreteTest is updated

--- src/test/java/com/company/test/utils/Constants.java
+++ src/test/java/com/company/test/utils/Constants.java
@@ -2,17 +2,17 @@ package com.company.test.utils;
 
 import org.openqa.selenium.By;
 
-public class Constants {
+public interface Constants {
 
-    public static final By NOMINATE_AS_MYSELF_BUTTON = By.xpath("//button[@class='js-np-recipient-item gf-btn btn-primary btn-lg']");
-    public static final By NOMINEE = By.xpath("//ul[@class='np-recipient-list list-unstyled clearfix']//h4[@data-title='Tony Kozlowsky']");
-    public static final By NEXT_BUTTON = By.xpath("//button[@class='js-np-next gf-btn btn-primary btn-block btn-lg inline-block']");
-    public static final By AWARD_PROGRAM = By.xpath("//div[@class='js-np-award-program-item np-award-program-default np-award-program relative']");
-    public static final By AWARD_REASON = By.xpath("//div[contains(@class, '1_bg_color')]");
-    public static final By SKIP_AWARD_ADVISOR = By.xpath("//a[@class='js-np-skip-advisor block np-advisor-skip-link award_reason_1_color']");
-    public static final By AWARD_TYPE = By.xpath("//div[3]/bdi");
-    public static final By AWARD_TITLE = By.xpath("//textarea[@id='np_awardTitle']");
-    public static final By AWARD_MESSAGE = By.xpath("//textarea[@id='np_awardMessage']");
-    public static final By SEND_AWARD_BUTTON = By.xpath("//button[@class='js-np-send-award gf-btn btn-primary btn-lg btn-block inline-block']");
-    public static final By CLOSE_BUTTON = By.xpath("//button[@class='js-np-close gf-btn btn-primary btn-block btn-lg inline-block']");
+    By NOMINATE_AS_MYSELF_BUTTON = By.xpath("//button[@class='js-np-recipient-item gf-btn btn-primary btn-lg']");
+    By NOMINEE = By.xpath("//ul[@class='np-recipient-list list-unstyled clearfix']//h4[@data-title='Tony Kozlowsky']");
+    By NEXT_BUTTON = By.xpath("//button[@class='js-np-next gf-btn btn-primary btn-block btn-lg inline-block']");
+    By AWARD_PROGRAM = By.xpath("//div[@class='js-np-award-program-item np-award-program-default np-award-program relative']");
+    By AWARD_REASON = By.xpath("//div[contains(@class, '1_bg_color')]");
+    By SKIP_AWARD_ADVISOR = By.xpath("//a[@class='js-np-skip-advisor block np-advisor-skip-link award_reason_1_color']");
+    By AWARD_TYPE = By.xpath("//div[3]/bdi");
+    By AWARD_TITLE = By.xpath("//textarea[@id='np_awardTitle']");
+    By AWARD_MESSAGE = By.xpath("//textarea[@id='np_awardMessage']");
+    By SEND_AWARD_BUTTON = By.xpath("//button[@class='js-np-send-award gf-btn btn-primary btn-lg btn-block inline-block']");
+    By CLOSE_BUTTON = By.xpath("//button[@class='js-np-close gf-btn btn-primary btn-block btn-lg inline-block']");
 }
